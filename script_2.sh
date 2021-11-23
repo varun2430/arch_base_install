e4d75c9f1c60cf97a5b887b9bf9aeedad26f355f
Varun Kadkade <varunkadkade2442@gmail.com>
2021-11-23 18:05:17 +0530

    tab issue

--- script_2.sh
+++ script_2.sh
@@ -15,9 +15,9 @@ echo "LANG=en_US.UTF-8" > /etc/locale.conf;
 echo "KEYMAP=us" > /etc/vconsole.conf;
 
 echo $hostname > /etc/hostname;
-echo "127.0.0.1 localhost" >> /etc/hosts;
-echo "::1               localhost" >> /etc/hosts;
-echo "127.0.1.1 $hostname.localdomain   $hostname" >> /etc/hosts;
+echo "127.0.0.1	localhost" >> /etc/hosts;
+echo "::1		localhost" >> /etc/hosts;
+echo "127.0.1.1	$hostname.localdomain	$hostname" >> /etc/hosts;
 
 clear;
 passwd;
