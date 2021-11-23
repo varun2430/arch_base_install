52a6921c0ab84090ab67b064396e8fb9f76120ee
Varun Kadkade <varunkadkade2442@gmail.com>
2021-11-23 17:51:36 +0530

    config changes moved to script_3

--- script_3.sh
+++ script_3.sh
@@ -1,6 +1,12 @@
 
 clear;
 
+echo "load-module module-switch-on-connect" >> /etc/pulse/default.pa;
+
+sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=3/g' /etc/default/grub;
+sed -i 's/loglevel=3 quiet/loglevel=3 quiet acpi_backlight=vendor/g' /etc/default/grub;
+grub-mkconfig -o /boot/grub/grub.cfg;
+
 pacman -S xf86-video-amdgpu;
 pacman -S xorg pavucontrol;
 
