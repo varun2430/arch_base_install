df9507da94e7b76af93bca776d9bb6e629a89059
Varun Kadkade <varunkadkade2442@gmail.com>
2021-11-23 11:13:46 +0530

    initial commit

---
+++ arch_base_install.sh
@@ -0,0 +1,38 @@
+
+echo "Username: "
+read username
+echo "Hostname: "
+read hostname
+
+ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+hwclock --systohc
+
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "KEYMAP=us" > /etc/vconsole.conf
+
+echo $hostname > /etc/hostname
+echo "127.0.0.1	localhost" >> /etc/hosts
+echo "::1		localhost" >> /etc/hosts
+echo "127.0.1.1	$hostname.localdomain	$hostname" >> /etc/hosts
+
+passwd
+
+pacman -S grub efibootmgr networkmanager wpa_supplicant dialog mtools dosfstools base-devel \
+	linux-headers bluez bluez-utils alsa-utils pulseaudio pulseaudio-bluetooth git \
+	reflector xdg-utils xdg-user-dirs
+
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable NetworkManager
+systemctl enable bluetooth
+
+EDITOR=nvim visudo
+
+useradd -m -G wheel $username
+passwd $username
+
+exit
+
