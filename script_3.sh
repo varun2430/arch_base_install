45dc7a5a798404f82f70fbe507ae77886e7f893b
Varun Kadkade <varunkadkade2442@gmail.com>
2021-11-23 15:31:44 +0530

    libinput touchpad config

---
+++ script_3.sh
@@ -0,0 +1,14 @@
+
+clear;
+
+pacman -S xf86-video-amdgpu;
+pacman -S xorg pavucontrol;
+
+echo 'Section "InputClass"
+    Identifier "DELL0A79:00 06CB:CE26 Touchpad"
+    Driver "libinput"
+    MatchIsTouchpad "on"
+    Option "Tapping" "on"
+    Option "NaturalScrolling" "true"
+
+EndSection' >> /etc/X11/xorg.conf.d/30-touchpad.conf;
